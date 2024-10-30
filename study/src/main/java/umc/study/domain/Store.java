b049cfe0f933b64d4d38150120aca6cba94ebe53
leeseulgi0208 <106726904+leeseulgi0208@users.noreply.github.com>
2024-10-31 06:06:10 +0900

    mission5

---
+++ study/src/main/java/umc/study/domain/Store.java
@@ -0,0 +1,27 @@
+package umc.study.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+}
